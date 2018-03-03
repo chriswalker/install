2810f2c20c160b0b81a54bb3d835a8f0c113bdfd
Chris Walker <thatamazingweb@gmail.com>
2018-03-03 19:43:46 +0000

    Set up brew + apps first; add go setup

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Install dotfiles
 cd ~
+
+# Install Brew + cmd line apps
+echo "Installing Homebrew...."
+usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Installing brew apps..."
+brew install neovim ack tree ctags fish python python3 tmux tree golang glide-golang dep node
+
+# Install dotfiles; neovim config requires python/python3 installation, so this is all done
+# after brew app installations
 echo "Creating .config directories...."
 mkdir -p ~/.config/fish
 mkdir -p ~/.config/nvim
@@ -12,14 +20,11 @@ echo "Running dotfiles install script..."
 echo "Downloading vim-plug..."
 curl -fLo ~/dotfiles/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "Installing neovim plugins..."
-
-# Install Brew + cmd line apps
-echo "Installing Homebrew...."
-usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-echo "Installing brew apps..."
-brew install neovim ack tree ctags fish python python3 tmux tree golang glide-golang
+nvim -c "PlugInstall | qa"
+nvim -c "GoInstallBinaries | qa"
 
 # Set up Dev workspace
+echo "Setting up Dev workspace..."
 mkdir -p ~/Dev/Applications
 mkdir -p ~/Dev/tmp
 mkdir -p ~/Dev/data
@@ -30,9 +35,13 @@ mkdir -p ~/Dev/Projects/Go/pkg
 mkdir -p ~Dev/Projects/Work
 
 # Install golang stuff
-echo "Installing go packages"
-# dep
-#micro/proto
-#micro/protoc-gen-go
-#openapi2proto (Karhoo fork)
-#openapi2jsonschema (chrusty)
+echo "Installing go packages..."
+# For work
+go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
+go get -u github/com/micro/protoc-gen-micro
+go get -u github.com/chrusty/protoc-gen-jsonschema
+go get -u github.com/karhoo/openapi2proto
+go get -u github.com/maxbrunsfeld/counterfeiter
+
+# General
+go get -u github.com/TrueFurby/go-callvis
