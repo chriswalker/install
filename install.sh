73d127843c13a70ba1a701d78a97b51fd1801846
Chris Walker <thatamazingweb@gmail.com>
2020-11-14 17:50:43 +0000

    Initial cut of Pi installation script

--- install.sh
+++ install.sh
@@ -9,17 +9,20 @@ cd $HOME
 # Package signing + repo for Tailscale
 curl https://pkgs.tailscale.com/stable/raspbian/buster.gpg | sudo apt-key add -
 curl https://pkgs.tailscale.com/stable/raspbian/buster.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+# Podman, Buildah and Skopeo
+curl https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/Raspbian_10/Release.key | sudo apt-key add -
+echo 'deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/Raspbian_10/ /' | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 
 # Install required software
 sudo apt update
-sudo apt install kakoune tmux ripgrep fd bat jq apt-transport-https tailscale
+sudo apt install kakoune tmux ripgrep fd bat jq apt-transport-https tailscale podman buildah skopeo
 
 # Start stuff up
 sudo tailscale up
 
-#
-# Go
-#
+# Shut things down - running Pis headless, and don't care about a bunch
+# of systemd services as a result. Stopping these decreases boot time.
+
 
 #
 # Move into new $HOME
